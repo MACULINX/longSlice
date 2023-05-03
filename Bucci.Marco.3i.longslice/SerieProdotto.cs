dd9e96d6821b327fc320e6b4622f58b72c951cb0
Marco <testrco4560@gmail.com>
2023-05-03 17:14:51 +0200

    Update SerieProdotto.cs

--- Bucci.Marco.3i.longslice/SerieProdotto.cs
+++ Bucci.Marco.3i.longslice/SerieProdotto.cs
@@ -1,37 +1,50 @@
 using System;
 public static class LargestSeriesval
 {
-   public static long GetLargestval(string digits, int span)
-{
-    if ( span > digits.Length || span < 0)
-        throw new ArgumentException();
-    
-    if (span == 0)
-        return 1;
-    
-    foreach (var c in digits)
-        if (!Char.IsDigit(c))
+    public static long GetLargestval(string digits, int span)
+    {
+
+        //Controllo che la sequenza non sia piu' grande della stringa o sia minore di 0
+        if (span > digits.Length || span < 0)
             throw new ArgumentException();
 
-    char[] DigitCharArray = digits.ToCharArray();
+        //Controllo che la sequenza non sia 0
+        if (span == 0)
+            return 1;
 
-    long MaxVal = 0;
+        //Converto la stringa in un char[] 
+        char[] DigitCharArray = digits.ToCharArray();
 
-    for (int i = 0; i <= digits.Length - span; i++)
-    {
-        long val = 1;
+        //Controllo che ogni carattere sia un numero
+        foreach (var c in DigitCharArray)
+            if (!Char.IsDigit(c))
+                throw new ArgumentException();
+        
+        //Dichiaro e inizializzo la variabile di ritorno "MaxVal"
+        int MaxVal = 0;
 
-        for (int j = 0; j < span; j++)
+        //Ciclo for per scorrere il vettore
+        for (int i = 0; i <= DigitCharArray.Length - span; i++)
         {
-            int digit = (int)DigitCharArray[i + j]-30;
-            val *= digit;
+            //Dichiaro e inizializzo la variabile da controllare "val"
+            int val = 1;
+            
+            //Ciclo for per moltiplicare la sequenza di numeri
+            for (int j = 0; j < span; j++)
+            {
+                //Qui avviene la conversione da char a int
+                int digit = (int)DigitCharArray[i + j] - 30;
+                //Qui avviene la moltiplicazione di ogni numero
+                val *= digit;
+            }
+
+            //Controllo che la variabile "val" sia maggiore di "MaxVal"
+            if (val > MaxVal)
+                //In caso positivo do a "MaxVal" il valore di "val"
+                MaxVal = val;
         }
-
-        if (val > MaxVal)
-            MaxVal = val;
+        //Ritorno "MaxVal"
+        return MaxVal;
     }
-
-    return MaxVal;
-}
 
 }
